21e29f2acf15a945484f70b992dbbaff7a21e5e0
Melle1010 <melle1010.m@gmail.com>
2026-04-24 10:24:20 +0200

    Add CORS policy extension and register in startup
    Created CorsPolicyExtension to centralize CORS setup with a "MainPolicy" allowing any origin, method, and header. Updated Program.cs to register this policy during service configuration.

---
+++ MovieLookUp/Extensions/CorsPolicyExtension.cs
@@ -0,0 +1,18 @@
+﻿namespace MovieLookUp.Extensions
+{
+    public static class CorsPolicyExtension
+    {
+        public static void AddCorsPolicy(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("MainPolicy", builder =>
+                {
+                    builder.AllowAnyOrigin()
+                           .AllowAnyMethod()
+                           .AllowAnyHeader();
+                });
+            });
+        }
+    }
+}
